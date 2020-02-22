9ebf3e82b55f413d53a349bdf935ee0a784144b3
ozer619 <prathameshd419@gmail.com>
2020-02-22 18:49:41 +0530

    final commit

--- book_review/models.py
+++ book_review/models.py
@@ -7,6 +7,7 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True,nullable=False)
     password_hash = db.Column(db.String(128),nullable=False)
+    reviews = db.relationship("Review", back_populates="user")    
 
     def __repr__(self):
         return '<User {}>'.format(self.username)  
@@ -19,7 +20,18 @@ class Books(db.Model):
     title = db.Column(db.String(150),nullable=False)
     author = db.Column(db.String(120),nullable=False)
     year = db.Column(db.Integer,nullable=False) 
-
+    
     def __repr__(self):
         return '<Books {}>'.format(self.title)  
+class Review(db.Model):
+    __tabelname__='review'
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey("users.id"),nullable=False)
+    book_id=db.Column(db.Integer,db.ForeignKey("books.id"),nullable=False)
+    rating=db.Column(db.Integer,nullable=False)
     
+    comment=db.Column(db.String(400),nullable=False)
+    user=db.relationship("User", back_populates="reviews")
+
+##db.session.query(User.username, Review.comment, Review.rating).filter_by(User.id=Review.user_id)import requests
+
--- book_review/routes.py
+++ book_review/routes.py
@@ -1,63 +1,66 @@
 from book_review import app
-from flask import flash, jsonify, redirect, render_template, request, url_for,session
+from flask import flash, jsonify, redirect, render_template, request, url_for, session
 from werkzeug.security import check_password_hash, generate_password_hash
 from book_review.models import *
 from book_review import db
-from flask_login import login_required,current_user,login_user,logout_user
+from flask_login import login_required, current_user, login_user, logout_user
 from flask_session import Session
 from sqlalchemy import or_
-
+import requests
+import os
 
 @app.route("/")
 @login_required
 def index():
-     return redirect("search")
+    return redirect("search")
 
-@app.route("/login",methods=["POST","GET"])
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
     session.clear()
     if request.method == "POST":
-        message=""
-        username=request.form.get("username")
-        password=request.form.get("password")
-        user=User.query.filter_by(username=username).first()
+        message = ""
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
         if not user:
-            message="username or password incorrect"
-            return render_template("apology.htm",message=message)
-        if not check_password_hash(user.password_hash,password):
-            message="username or password incorrect"
-            return render_template("apology.htm",message=message)
-           
-        login_user(user,remember=False)
+            message = "username or password incorrect"
+            return render_template("apology.htm", message=message)
+        if not check_password_hash(user.password_hash, password):
+            message = "username or password incorrect"
+            return render_template("apology.htm", message=message)
+
+        login_user(user, remember=False)
         # Redirect user to home page
         return redirect("/")
     else:
         return render_template("login.htm")
 
-@app.route("/register",methods=["POST","GET"])
+
+@app.route("/register", methods=["POST", "GET"])
 def register():
     """Register user"""
     if request.method == "POST":
-        message=""
-        username= request.form.get("username")
-        password= request.form.get("password")
+        message = ""
+        username = request.form.get("username")
+        password = request.form.get("password")
         if not password:
-            message="missing password"
-            return render_template("apology.htm",message=message)
-        password=generate_password_hash(password)
+            message = "missing password"
+            return render_template("apology.htm", message=message)
+        password = generate_password_hash(password)
         print(password)
-        confirm_password=request.form.get("confirmation")
+        confirm_password = request.form.get("confirmation")
         if not username:
-            message="missing username"
-            return render_template("apology.htm",message=message)
+            message = "missing username"
+            return render_template("apology.htm", message=message)
         if not confirm_password:
-            message="missing confirm-password"
-            return render_template("apology.htm",message=message)
-        if not request.form.get("password")==confirm_password:
-            message="password and confirm-password field donot match"
-            return render_template("apology.htm",message=message)
+            message = "missing confirm-password"
+            return render_template("apology.htm", message=message)
+        if not request.form.get("password") == confirm_password:
+            message = "password and confirm-password field donot match"
+            return render_template("apology.htm", message=message)
 
-        new_user=User(username=username,password_hash=password)
+        new_user = User(username=username, password_hash=password)
         db.session.add(new_user)
         db.session.commit()
         return redirect("/login")
@@ -77,20 +80,77 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-@app.route("/search",methods=["POST","GET"])
+
+@app.route("/search", methods=["POST", "GET"])
 @login_required
 def search():
     if request.method == "POST":
-        flag=1
+        flag = 1
         search = request.form.get("search")
-        books=Books.query.filter(or_(Books.author.like('%' + search + '%'),
-        Books.title.like('%' + search + '%'),
-        Books.isbn.like('%' + search + '%'))).all()
+        books = Books.query.filter(or_(Books.author.like('%' + search + '%'),
+                                       Books.title.like('%' + search + '%'),
+                                       Books.isbn.like('%' + search + '%'))).all()
         if not books:
-            message="no results found"
-            return render_template("apology.htm",message=message)
-     
-        return render_template("search.htm",flag=flag,books=books)
+            message = "no results found"
+            return render_template("apology.htm", message=message)
+
+        return render_template("search.htm", flag=flag, books=books)
     else:
-        flag=0
-        return render_template("search.htm",flag=flag) 
+        flag = 0
+        return render_template("search.htm", flag=flag)
+
+
+@app.route('/review/<int:book_id>', methods=["GET"])
+@login_required
+def review(book_id):
+ 
+    kitab = Books.query.get(book_id)
+    session['book_id'] = book_id
+    review_of_book = Review.query.filter_by(book_id=book_id)
+    isbns=db.session.query(Books.isbn).filter(Books.id==book_id).all()
+    isbns=str(isbns[0][0])
+
+    review_of_book = db.session.query(User, Review.comment, Review.rating).filter(
+        Review.book_id == session["book_id"]).join(Review, User.id == Review.user_id).all()
+    if len(isbns)==10:
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key":"mA2E8ydIgLk4hEdtw57DXA", "isbns":isbns }).json()
+        
+    if(len(isbns)==9):
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key":"mA2E8ydIgLk4hEdtw57DXA", "isbns":'0' + isbns }).json()
+    if(len(isbns)==8):
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key":"mA2E8ydIgLk4hEdtw57DXA", "isbns":'00' + isbns }).json()
+    if(len(isbns)==7):
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key":"mA2E8ydIgLk4hEdtw57DXA", "isbns":'000' + isbns }).json()    
+        
+    
+  
+    return render_template("review.htm", kitab=kitab, book_id=book_id, review_of_book=review_of_book,res=res)
+
+
+@app.route('/review', methods=["POST"])
+@login_required
+def review_1():
+    book_id = session['book_id']
+
+    rating_b = request.form.get("rating_b")
+    rating_t = request.form.get("rating_t")
+
+    if not rating_b:
+        message = "Rating not provided"
+        return render_template("apology.htm", message=message)
+    if not rating_t:
+        message = "comment not provided"
+        return render_template("apology.htm", message=message)
+    if (rating_b != '1' and rating_b != '2' and rating_b != '3' and rating_b != '4' and rating_b != '5'):
+        message = "invalid rating"
+        return render_template("apology.htm", message=message)
+
+    else:
+        new_rating = Review(user_id=current_user.id,
+                            book_id=book_id, rating=rating_b, comment=rating_t)
+        db.session.add(new_rating)
+        db.session.commit()
+
+    return redirect(url_for('review', book_id=session["book_id"]))
+
+ 
